1e2698e8bb6fe3bc2fdfb63784c6aaa98e152caf
Justin Collins <jbc0981@gmail.com>
2018-07-30 19:23:52 +0000

    Updating config/application to possibly fix deploying assets to heroku prod

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module AlphaBlog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    #Adding for possible fix to deploy assets to heroku. - JC - 07/30/18
   end
 end
